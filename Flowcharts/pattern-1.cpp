72b424114bb96c12dce29868ec30d3a28894ca40
cyberkartik <cyberkartik@gmail.com>
2021-11-27 16:08:12 +0530

    Pattern

---
+++ Flowcharts/pattern-1.cpp
@@ -0,0 +1,36 @@
+//     1
+//    123
+//   12345
+//  1234567
+// 123456789
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n, row, cs, in;
+  cin>>n;
+  row = 1;
+
+  while (row<=n)
+  {
+      //spaces
+      cs = 1;
+      while(cs<=(n-row))
+      {
+          cout<<' ';
+          cs = cs+1;
+      }
+
+      //no.
+      in = 1;
+      while(in <= (2*row)-1)
+      {
+          cout<<in;
+          in = in + 1 ;
+      }
+      cout<<endl;
+      row = row +1;
+  }
+  return 0;
+}
---
+++ Flowcharts/pattern.cpp
@@ -0,0 +1,47 @@
+//    1
+//   232
+//  34543
+// 4567654
+//567898765
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row, count, count1, count2, n, print, print1;
+    cin>>n ;
+    row = 1;
+
+    while(row<=n)
+    {
+        count = 1;
+        while(count <= (n-row) )
+        {
+            cout<<" ";
+            count = count + 1;
+        }
+        count1 = 1;
+        print = row;
+
+        while(count1 <= row )
+        {
+            cout << print;
+            count1 = count1 + 1;
+            print = print +1;
+        }
+        count2 = 1;
+        print1 = (2*row)-2;
+
+        while(count2 <= (row-1) )
+        {
+            cout << print1;
+            count2 = count2 + 1;
+            print1 = print1 - 1;
+        }
+        row = row +1;
+        cout << endl;
+    }
+    return 0;
+}
